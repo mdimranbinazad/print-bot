dafc9e733397f1e34590dee8442952bbf7e131df
Mohammad Samiul Islam <forthright48@gmail.com>
2017-11-05 18:54:57 +0600

    Add information on top of text box

--- middlewares/passSession.js
+++ middlewares/passSession.js
@@ -1,8 +1,10 @@
 const _ = require('lodash');
 
 module.exports = function(req, res, next) {
-  _.forEach(req.session, function(value, key){
-    res.locals[key] = value;
-  })
+  res.locals.login = req.session.login;
+  res.locals.username = req.session.username ;
+  res.locals.status = req.session.status;
+  res.locals.totalPageLimit = req.session.totalPageLimit;
+  res.locals.pagePrinted = req.session.pagePrinted;
   next();
 }
